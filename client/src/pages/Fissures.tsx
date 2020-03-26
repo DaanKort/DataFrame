a27437c2aa143f7435ae8de3626807dd2b9b3b80
DaanKort <daankort3@gmail.com>
2020-03-26 13:41:41 +0100

    Added newest changes

--- client/src/pages/Fissures.tsx
+++ client/src/pages/Fissures.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { IFissures } from '../interfaces/index';
+import { requestFissures } from '../actions/actions';
 
 export default function Fissures() {
-    return (
-        <div>
-            Hello, this fissures.
-        </div>
-    )
+  const fissures = useSelector<IFissures, any>(state => state.fissures)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(requestFissures());
+  }, [dispatch]);
+
+  let fissuresData = fissures.fissures;
+
+  (() => {
+    const compare = (a: any, b: any) => {
+      if (a.tierNum > b.tierNum) return 1;
+      if (b.tierNum > a.tierNum) return -1;
+      return 0;
+    }
+
+    fissuresData.sort(compare);
+  })()
+
+  return (
+    <section className='fissures boxed'>
+      {
+        fissuresData.sort().map((fissure: any, index: any) => (
+          <article className='fissures-container' key={index}>
+            <div className='fissures-inner'>
+              <p className='fissures__tier'>T{fissure.tierNum}</p>
+              <h3 className='fissures__node'>{fissure.node}</h3>
+              <h5 className='fissures__mission'>{fissure.enemy} - {fissure.missionType}</h5>
+              <h6 className='fissures__era'>{fissure.tier}</h6>
+              <p className='fissures__eta'>{fissure.eta}</p>
+            </div>
+          </article>
+        ))
+      }
+    </section>
+  )
 }
