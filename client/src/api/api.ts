2da1e2fc1133656b6ad8c711126c4e15c259acaf
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2020-03-30 10:54:51 +0200

    returned regular json

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,8 +2,7 @@ const baseUrl = 'https://api.warframestat.us/';
 export const api = async(endpoint: string): Promise<any> => {
   try {
     const response = await fetch(baseUrl + endpoint);
-    const dataObj = response.json()
-    return dataObj;
+    return response.json()
   } catch (e) {
     console.log(e)
   }
