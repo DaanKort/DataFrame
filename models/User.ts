91aeb88646c9b7651c09659bde7bdfb33586b641
daankort <daankort3@gmail.com>
2020-03-31 09:12:57 +0200

    FT-05: Cleaned up code

--- models/User.ts
+++ models/User.ts
@@ -5,23 +5,19 @@ const UserSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: true,
-        default: ''
     },
     lastName: {
         type: String,
         required: true,
-        default: ''
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        default: ''
     },
     password: {
         type: String,
         required: true,
-        default: ''
     },
     date: {
         type: Date,
--- routes/api/user.ts
+++ routes/api/user.ts
@@ -6,66 +6,34 @@ const User = require('../../models/User');
 
 //@route POST api/user
 router.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const newUser = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    password: req.body.password,
-    email: req.body.email,
-  });
-  if (!newUser.firstName) {
-    return res.send({
-      success: false,
-      message: "Firstname cannot be empty"
-    });
-  }
-  if (!newUser.lastName) {
-    return res.send({
-      success: false,
-      message: "Last Name cannot be empty"
-    });
-  }
-  if (!newUser.email) {
-    return res.send({
-      success: false,
-      message: "Email cannot be empty"
-    });
-  }
-  if (!newUser.password) {
-    return res.send({
-      success: false,
-      message: "Password cannot be empty"
-    });
-  }
- 
 
-  newUser.email = newUser.email.toLowerCase(),
-  newUser.firstName = newUser.firstName,
-  newUser.lastName = newUser.lastName,
-  newUser.password = newUser.generateHash(newUser.password),
-  User.find({
-    email: newUser.email
-  }, (err:any, user:any) => {
-    if(err) {
+  const { firstName, lastName, email, password } = req.body;
+
+  //Field validation
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({
+      message: 'Please enter all fields.'
+    })
+  }
+
+  //Check for existing user 
+  User.findOne({ email }).then(user => {
+    if (user) return res.status(400).json({ message: "User with this email already exists" })
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password
+    });
+
+    newUser.save().then(() => {
       return res.send({
-        success: false,
-        message: "Error: Server error"
+        singedUp: true,
+        message: "Signed up!"
       });
-    }
-    if(user.length > 0) {
-      return res.send({
-        success: false,
-        message: "User with this email already exists"
-      });
-    } else {
-      newUser.save().then(() => {
-        return res.send({
-          success: true,
-          message: "Signed up!"
-        });
-      });
-    }
+    });
   })
 });
-
 module.exports = router;
 
