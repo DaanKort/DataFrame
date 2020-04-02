c45b7f0a98294ad23b77e8b7c75df4a63bb16c91
Matthijs Hoek <33547988+MatthijsHoek@users.noreply.github.com>
2020-04-02 15:43:53 +0200

    FT 12 home (#5)
    * Implemented news into home
    
    * Added invasions and and fissures to homepage
    
    * changed faulty font-size
    
    * Added alerts to home
    
    * Added new layout to home sections
    
    * Fixed fissure layout
    
    * evenly spaced invasions & fissures
    
    * Added button to fissures section
    
    * added alt prop to images
    
    * removed all unnecessary imports and data requests
    
    * Evened out spacing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,73 +1,49 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { requestWeaponData, requestNews, requestFrameData, requestMods, requestArcanes, requestResources, requestCetusCycle, requestVallisCycle, requestAlerts, requestEvents, requestDeals, requestFissures, requestSorties, requestInvasions } from '../actions/actions'
-import { IWeapons, INews, IFrames, IMods, IArcanes, IResources, ICetusCycle, IVallisCycle, IAlerts, IEvents, IDailyDeals, ISorties, IFissures, IInvasions } from '../interfaces/index';
+import { requestNews, requestAlerts, requestFissures, requestInvasions } from '../actions/actions'
+import { INews, IAlerts, IFissures, IInvasions } from '../interfaces/index';
 import Glider from '../components/glider/index';
 import Button from '../components/button/index';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle, faSlash } from '@fortawesome/free-solid-svg-icons';
+import Injector from '../assets/images/injector.png';
+import Fieldron from '../assets/images/fieldron.png';
+import Mutagen from '../assets/images/mutagen.png';
 
 export default function Home() {
   const news = useSelector<INews, any>(state => state.news)
-  const weapons = useSelector<IWeapons, any>(state => state.weapons)
-  const frames = useSelector<IFrames, any>(state => state.frames)
-  const mods = useSelector<IMods, any>(state => state.mods)
-  const arcanes = useSelector<IArcanes, any>(state => state.arcanes)
-  const resources = useSelector<IResources, any>(state => state.resources)
-  const cetusCycle = useSelector<ICetusCycle, any>(state => state.cetusCycle)
-  const vallisCycle = useSelector<IVallisCycle, any>(state => state.vallisCycle)
   const alerts = useSelector<IAlerts, any>(state => state.alerts)
-  const events = useSelector<IEvents, any>(state => state.events)
-  const deals = useSelector<IDailyDeals, any>(state => state.deals)
-  const sorties = useSelector<ISorties, any>(state => state.sorties)
   const fissures = useSelector<IFissures, any>(state => state.fissures)
   const invasions = useSelector<IInvasions, any>(state => state.invasions)
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(requestNews());
-    dispatch(requestWeaponData());
-    dispatch(requestFrameData());
-    dispatch(requestMods());
-    dispatch(requestArcanes());
-    dispatch(requestResources());
-    dispatch(requestCetusCycle());
-    dispatch(requestVallisCycle());
     dispatch(requestAlerts());
-    dispatch(requestEvents());
-    dispatch(requestDeals());
     dispatch(requestFissures());
-    dispatch(requestSorties());
     dispatch(requestInvasions());
   }, [dispatch]);
 
-  let weaponData = weapons.weapons
-  let newsData = news.news
-  let frameData = frames.frames
-  let modData = mods.mods
-  let arcaneData = arcanes.arcanes
-  let resourceData = resources.resources
-  let cetusCycleData = cetusCycle
-  let vallisCycleData = vallisCycle
-  let alertsData = alerts.alerts
-  let eventData = events.events
-  let dealsData = deals.deals
-  let sortiesData = sorties.sorties
-  let fissureData = fissures.fissures
-  let invasionsData = invasions.invasions
+  let newsData = news.news;
+  let alertsData = alerts.alerts;
+  let fissureData = fissures.fissures;
+  let invasionsData = invasions.invasions;
 
-  console.log('News: ', newsData);
-  console.log('Weapons: ', weaponData);
-  console.log('Frames: ', frameData);
-  console.log('Mods: ', modData);
-  console.log('Arcanes: ', arcaneData);
-  console.log('Recources: ', resourceData);
-  console.log('Cetus: ', cetusCycleData);
-  console.log('Vallis: ', vallisCycleData);
-  console.log('Alerts: ', alertsData);
-  console.log('Events: ', eventData);
-  console.log('Deals: ', dealsData);
-  console.log('Fissures: ', fissureData);
-  console.log('Sorties: ', sortiesData);
-  console.log('Invasions: ', invasionsData);
+  (() => {
+    for (let i = invasionsData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [invasionsData[i], invasionsData[j]] = [invasionsData[j], invasionsData[i]];
+    }
+    return invasionsData;
+  })();
+
+  (() => {
+    for (let i = fissureData.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [fissureData[i], fissureData[j]] = [fissureData[j], fissureData[i]];
+    }
+    return fissureData;
+  })();
 
   const gliderSettings = {
     dots: true,
@@ -145,7 +121,83 @@ export default function Home() {
             </div>
           </div>
         </Glider>
-        <div className='home-cards boxed'>
+        <div className='home-duo home--spacing boxed'>
+          <div className='home-side'>
+            <h4 className='home-side__title'>Latest News:</h4>
+            {
+              newsData.reverse().slice(0, 1).map((news:any, index:any) => (
+                <div className='home-side__container' key={index}>
+                  <img src={news.imageLink} className='home-side__image' alt='asset' />
+                  <h5 className='home-side__title'>{news.message}</h5>
+                </div>
+              ))
+            }
+          </div>
+          <div className='home-side'>
+            {alertsData.length === 0 ? (
+              <div className='home-side__empty'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='home-side__icon' />
+                <h4>There are no alerts at the moment!</h4>
+              </div>
+            ) : (
+              alertsData.map((alert:any, index:any) => (
+                <div className='home-side__data' key={index}>
+                  ...alerts
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+
+        <div className='home-invasions'>
+          <div className='home-invasions__wrapper boxed'>
+            <h3 className='home-invasions__title'>Invasions</h3>
+            <div className='home-invasions__layout'>
+              <div className='home-invasions__info'>
+                {
+                  invasionsData.slice(0, 4).map((invasion:any, index:any) => (
+                    <div className='home-invasions__container' key={index}>
+                      <h4 className='home-invasions__node'>{invasion.node}</h4>
+                      <h5 className='home-invasions__desc'>{invasion.desc}</h5>
+                      <h6 className='home-invasions__eta'>{invasion.eta}</h6>
+                    </div>
+                  ))
+                }
+              </div>
+              <div className='home-invasions__collage'>
+                <div className='home-invasions__inner'>
+                  <h4>Potential Rewards:</h4>
+                  <img src={Injector} className='home-invasions__image' alt='asset'/>
+                  <img src={Fieldron} className='home-invasions__image' alt='asset'/>
+                  <img src={Mutagen} className='home-invasions__image' alt='asset'/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='home-invasions__wrap'>
+            <Button buttonText='Visit' buttonLink='/invasions' buttonClass='button-gold home-invasions__button' />
+          </div>
+        </div>
+
+        <div className='home-fissures home--spacing boxed'>
+          <h3>Fissures</h3>
+          <FontAwesomeIcon icon={faSlash} className='home-fissures__icon' />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <div className='home-fissures__layout'>
+            {
+              fissureData.slice(0, 3).map((fissure:any, index:any) => (
+                <div className='home-fissures__container' key={index}>
+                  <div className='home-fissures__wrapper'>
+                    <h5>{fissure.tier} T{fissure.tierNum}</h5>
+                    <h6>{fissure.node}</h6>
+                    <h6>{fissure.missionType} - {fissure.enemy}</h6>
+                    <h6>{fissure.eta}</h6>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          <Button buttonText='Visit' buttonLink='/fissures' buttonClass='button-gold home-fissures__button' />
         </div>
       </section>
     </>
