45546e15db8ac2a3de9515e089aa26980ea64a5c
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2020-04-02 11:26:14 +0200

    Added new layout to home sections

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,10 @@ import { INews, ICetusCycle, IVallisCycle, IAlerts, IEvents, IDailyDeals, IFissu
 import Glider from '../components/glider/index';
 import Button from '../components/button/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { faExclamationTriangle, faSlash } from '@fortawesome/free-solid-svg-icons';
+import Injector from '../assets/images/injector.png';
+import Fieldron from '../assets/images/fieldron.png';
+import Mutagen from '../assets/images/mutagen.png';
 
 export default function Home() {
   const news = useSelector<INews, any>(state => state.news)
@@ -130,81 +133,80 @@ export default function Home() {
             </div>
           </div>
         </Glider>
-        <div className='home-news home--spacing boxed'>
-          {
-            newsData.reverse().slice(0, 2).map((news:any, index:any) => (
-              <div className='home-news__container' key={index}>
-                <img src={news.imageLink} className='home-news__image' alt='asset' />
-                <h5 className='home-news__title'>{news.message}</h5>
-              </div>
-            ))
-          }
-        </div>
-        <div className='home-invasions'>
-          <div className='home-invasions__wrapper boxed'>
+        <div className='home-duo home--spacing boxed'>
+          <div className='home-side'>
             {
-              invasionsData.slice(0, 2).map((invasion:any, index:any) => (
-                <div className='home-invasions__container' key={index}>
-                  <h4 className='home-invasions__node'>{invasion.node}</h4>
-                  <h5 className='home-invasions__desc'>{invasion.desc}</h5>
-                  <h6 className='home-invasions__eta'>{invasion.eta}</h6>
-                  <div className='home-invasions-sides'>
-                      {
-                        invasion.attackerReward.itemString && (
-                          <div className='home-invasions__side'>
-                            <img src={invasion.attackerReward.thumbnail} className='home-invasions__image' alt='asset' />
-                            <h5>Attack Reward:</h5>
-                            <h6>{invasion.attackerReward.itemString}</h6>
-                          </div>
-                        )
-                      }
-                      {
-                        invasion.defenderReward.itemString && (
-                          <div className='home-invasions__side'>
-                            <img src={invasion.defenderReward.thumbnail} className='home-invasions__image' alt='asset' />
-                            <h5>Defend Reward:</h5>
-                            <h6>{invasion.defenderReward.itemString}</h6>
-                          </div>
-                        )
-                      }
-                  </div>
+              newsData.reverse().slice(0, 1).map((news:any, index:any) => (
+                <div className='home-side__container' key={index}>
+                  <img src={news.imageLink} className='home-side__image' alt='asset' />
+                  <h5 className='home-side__title'>{news.message}</h5>
                 </div>
               ))
             }
+          </div>
+          <div className='home-side'>
+            {alertsData.length === 0 ? (
+              <div className='home-side__empty'>
+                <FontAwesomeIcon icon={faExclamationTriangle} className='home-side__icon' />
+                <h4>There are no alerts at the moment!</h4>
+              </div>
+            ) : (
+              alertsData.map((alert:any, index:any) => (
+                <div className='home-side__data' key={index}>
+                  ...alerts
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+
+        <div className='home-invasions'>
+          <div className='home-invasions__wrapper boxed'>
+            <h3 className='home-invasions__title'>Invasions</h3>
+            <div className='home-invasions__layout'>
+              <div className='home-invasions__info'>
+                {
+                  invasionsData.slice(0, 4).map((invasion:any, index:any) => (
+                    <div className='home-invasions__container' key={index}>
+                      <h4 className='home-invasions__node'>{invasion.node}</h4>
+                      <h5 className='home-invasions__desc'>{invasion.desc}</h5>
+                      <h6 className='home-invasions__eta'>{invasion.eta}</h6>
+                    </div>
+                  ))
+                }
+              </div>
+              <div className='home-invasions__collage'>
+                <div className='home-invasions__inner'>
+                  <h4>Potential Rewards:</h4>
+                  <img src={Injector} className='home-invasions__image' />
+                  <img src={Fieldron} className='home-invasions__image' />
+                  <img src={Mutagen} className='home-invasions__image' />
+                </div>
+              </div>
+            </div>
           </div>
           <div className='home-invasions__wrap'>
             <Button buttonText='Visit' buttonLink='/invasions' buttonClass='button-gold home-invasions__button' />
           </div>
         </div>
+
         <div className='home-fissures home--spacing boxed'>
-          {console.log(fissureData)}
-          {
-            fissureData.slice(0, 3).map((fissure:any, index:any) => (
-              <div className='home-fissures__container' key={index}>
-                <div className='home-fissures__wrapper'>
-                  <h5>{fissure.node}</h5>
-                  <h5>{fissure.tier} T{fissure.tierNum}</h5>
-                  <h6>{fissure.missionType} - {fissure.enemy}</h6>
-                  <h6>{fissure.eta}</h6>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-        <div className='home-alerts home--spacing'>
-          <div className='home-alerts__wrapper boxed'>
-              {alertsData.length === 0 ? (
-                <div className='home-alerts__empty'>
-                  <FontAwesomeIcon icon={faExclamationTriangle} className='home-alerts__icon' />
-                  <h4>There are no alerts at the moment!</h4>
-                </div>
-              ) : (
-                alertsData.map((alert:any, index:any) => (
-                  <div className='home-alerts__data' key={index}>
-                    ...alerts
+          <h3>Fissures</h3>
+          <FontAwesomeIcon icon={faSlash} className='home-fissures__icon' />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <div className='home-fissures__layout'>
+            {
+              fissureData.slice(0, 3).map((fissure:any, index:any) => (
+                <div className='home-fissures__container' key={index}>
+                  <div className='home-fissures__wrapper'>
+                    <h5>{fissure.node}</h5>
+                    <h5>{fissure.tier} T{fissure.tierNum}</h5>
+                    <h6>{fissure.missionType} - {fissure.enemy}</h6>
+                    <h6>{fissure.eta}</h6>
                   </div>
-                ))
-              )}
+                </div>
+              ))
+            }
           </div>
         </div>
       </section>
