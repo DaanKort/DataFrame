221c64da706c38854684e00b42d4d83ff2895008
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2020-04-03 11:17:01 +0200

    Started on invasions page

--- client/src/pages/Invasions.tsx
+++ client/src/pages/Invasions.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Invasions() {
-    return (
-        <div>
-            Hello, this invasions.
-        </div>
-    )
+  return (
+    <>
+      <section className='invasions boxed'>
+        <h2 className='invasions__title'>Invasions</h2>
+      </section>
+    </>
+  )
 }
